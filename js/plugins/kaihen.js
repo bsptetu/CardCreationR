9af0b0fcfdaa1d7e4371cfc904caf67be74c94b6
bsptetu <55188675+bsptetu@users.noreply.github.com>
2021-02-07 21:59:46 +0900

    20210207ll

--- js/plugins/kaihen.js
+++ js/plugins/kaihen.js
@@ -198,3 +198,14 @@ Sprite_Enemy.prototype.startCollapse = function() {
     this._effectDuration = 32;
     this._appeared = false;
 };
+
+ImageManager.loadBitmap = function(folder, filename, hue, smooth) {
+    if (filename) {
+        var path = folder + encodeURIComponent(filename) + '.png';
+        var bitmap = this.loadNormalBitmap(path, hue || 0);
+        bitmap.smooth = false;
+        return bitmap;
+    } else {
+        return this.loadEmptyBitmap();
+    }
+};
